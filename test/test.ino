e46f83cbf36309e3df64f247cf1cf420248eecbf
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2023-08-02 00:06:26 +0900

    Create test.ino

---
+++ test/test.ino
@@ -0,0 +1,17 @@
+#include <legangle.h>
+void setup() {
+  // put your setup code here, to run once:
+  setservos();
+  leg1(0, 0);
+  leg2(0, 0);
+  leg3(0, 0);
+  leg4(0, 0);
+  leg5(0, 0);
+  leg6(0, 0);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
