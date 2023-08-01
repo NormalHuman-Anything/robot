712eb6e726c475b8fc57706a4432675b38a8a11b
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2023-08-02 00:11:29 +0900

    쌤 이거 돌려주세요

--- test/test.ino
+++ test/test.ino
@@ -3,15 +3,28 @@ void setup() {
   // put your setup code here, to run once:
   setservos();
   leg1(0, 0);
-  leg2(0, 0);
+  leg2(60, 0);
   leg3(0, 0);
   leg4(0, 0);
-  leg5(0, 0);
+  leg5(60, 0);
   leg6(0, 0);
   delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  leg1(-10, 0);
+  leg4(30, 0);
+  delay(300);
 
+  leg4(30, 30);
+  delay(300);
+
+  leg1(0, 0);
+  leg4(0, 30);
+  delay(300);
+
+  leg4(-10, 30);
+  leg1(30, 30);
+  delay(30000000);
 }
