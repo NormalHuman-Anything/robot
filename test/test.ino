09b409e1a1e62af3c5f534dd8a0fe6d15ac13d42
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2023-08-02 00:17:16 +0900

    어쩌면 될거같은

--- test/test.ino
+++ test/test.ino
@@ -15,16 +15,44 @@ void loop() {
   // put your main code here, to run repeatedly:
   leg1(-10, 0);
   leg4(30, 0);
-  delay(300);
+  delay(500);
 
   leg4(30, 30);
-  delay(300);
+  delay(500);
 
   leg1(0, 0);
   leg4(0, 30);
-  delay(300);
+  delay(500);
 
   leg4(-10, 30);
   leg1(30, 30);
   delay(30000000);
+
+  leg3(-10, 0);
+  leg6(30, 0);
+  delay(500);
+
+  leg6(30, 30);
+  delay(500);
+
+  leg3(0, 0);
+  leg6(0, 30);
+  delay(500);
+
+  leg6(-10, 30);
+  leg3(30, 0);
+  delay(500);
+
+  leg3(30, 30);
+  delay(500);
+
+  leg3(0, 30);
+  leg6(0, 30);
+  delay(500);
+
+  leg1(0, 0);
+  leg3(0, 0);
+  leg4(0, 0);
+  leg6(0, 0);
+  delay(500);
 }
