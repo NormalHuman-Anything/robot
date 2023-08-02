8fe2b89ffa4b35f4b5e7474bba91a1420f0d81e7
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2023-08-02 09:16:21 +0900

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -8,51 +8,51 @@ void setup() {
   leg4(0, 0);
   leg5(60, 0);
   leg6(0, 0);
-  delay(1000);
+  delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  leg1(-30, 0);
-  leg4(30, 0);
-  delay(500);
+  leg1(-40, 0);
+  leg4(40, 0);
+  delay(1000);
 
-  leg4(4x 0, 30);
-  delay(500);
+  leg4(40, 30);
+  delay(1000);
 
   leg1(0, 0);
   leg4(0, 30);
-  delay(500);
+  delay(1000);
 
-  leg4(-30, 30);
+  leg4(-40, 30);
   leg1(40, 30);
-  delay(500);
+  delay(1000);
 
-  leg3(-30, 0);
+  leg3(-40, 0);
   leg6(40, 0);
-  delay(500);
+  delay(1000);
 
-  leg6(30, 30);
-  delay(500);
+  leg6(40, 30);
+  delay(1000);
 
   leg3(0, 0);
   leg6(0, 30);
-  delay(500);
+  delay(1000);
 
-  leg6(-10, 30);
-  leg3(30, 0);
-  delay(500);
+  leg6(-40, 30);
+  leg3(40, 0);
+  delay(1000);
 
-  leg3(30, 30);
-  delay(500);
+  leg3(40, 30);
+  delay(1000);
 
   leg3(0, 30);
   leg6(0, 30);
-  delay(500);
+  delay(1000);
 
   leg1(0, 0);
   leg3(0, 0);
   leg4(0, 0);
   leg6(0, 0);
-  delay(500);
+  delay(1000);
 }
